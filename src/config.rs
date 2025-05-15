a2705f7235c550ffdcce24c8238ac6b537ea565d
AndyWu <124117191+MrWu2888@users.noreply.github.com>
2025-05-16 00:41:32 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.1.9"];
+pub const RS_PUB_KEY: &str = "M8IhyUSzMIW8k8gzFmNaKEThZNY57Mb2GDjZ2VhUVmY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
